e0952483c68ad0c3772113d6f8a9356986185f15
dq-aem-shravan <shravanm@digiquadsolutions.com>
2025-10-10 18:17:04 +0530

    url for employee

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -2,7 +2,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://192.168.1.42:8081/web/api/v1",
+  baseURL: "http://192.168.1.21:8081/web/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
